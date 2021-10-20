7715adf967ecde62fbac361da68ea875de4e1e08
Angel <mr.angelantoniolopez@gmail.com>
2021-10-20 10:55:41 -0600

    paquetes

---
+++ src/java/conexion/TestConexion.java
@@ -0,0 +1,10 @@
+
+package conexion;
+
+public class TestConexion {
+    public static void main(String[] args) {
+        Conexion c = new Conexion();
+        c.Conectar();
+        c.Desconectar();
+    }
+}
